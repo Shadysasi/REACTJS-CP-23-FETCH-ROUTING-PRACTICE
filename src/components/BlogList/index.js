b47455e91b824649ba5df2eff69014626409a63e
Sasikumar <sasieng5851@gmail.com>
2023-11-07 12:13:48 +0000

    fetch and routing practice

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,45 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import BlogItem from '../BlogItem'
+import './index.css'
+
+class BlogList extends Component {
+  state = {blogsData: [], isLoading: true}
+
+  componentDidMount() {
+    this.getBlogsData()
+  }
+
+  getBlogsData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    const updateData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatar_url,
+      author: eachItem.author,
+      topic: eachItem.topic,
+    }))
+    this.setState({blogsData: updateData, isLoading: false})
+  }
+
+  render() {
+    const {blogsData, isLoading} = this.state
+    return (
+      <div className="blog-list-container">
+        {isLoading ? (
+          <div data-testid="loader">
+            <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+          </div>
+        ) : (
+          blogsData.map(eachBlog => (
+            <BlogItem key={eachBlog.id} blogData={eachBlog} />
+          ))
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogList
